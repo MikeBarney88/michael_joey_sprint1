9ae1df7de1ae83b9c910a154e1e0b8c025423d0b
Joey <jowhiey@gmail.com>
2025-02-21 13:18:07 -0330

    the form has been formed

--- scripts/script.js
+++ scripts/script.js
@@ -46,12 +46,13 @@ window.addEventListener("DOMContentLoaded", function() {
         document.querySelector("#oCredit").value = "";
 
         setTimeout(() => {
-            oError.innerHTML = "";
+            oError.style.backgroundColor = "";
+            oError.innerHTML = "&nbsp;";
         } , 3000)
     }
 
 
-    //Add to Order button:
+    //Add to Order button (Menu Page):
     // let addtoOrdBtn = document.querySelector("#addtoOrdBtn");
     // addtoOrdBtn.addEventListener("click", function() {
     //     //On click, try to go back to the element with the name of the item and add it to ordList in localStorage.
@@ -87,41 +88,82 @@ window.addEventListener("DOMContentLoaded", function() {
     });
 
     //Update order and set up choose items field:
-    let oItems = [];
 
     document.querySelector("#oItemPick").addEventListener("change", function(e) {
-        document.querySelector("#oQuantity").setAttribute("required", true);
-
-        oItems.push(document.querySelector("#oItemPick"));
+        document.querySelector("#oQuantity").setAttribute("required", true);    
 
         e.preventDefault();
     })
 
 
+    document.querySelector("#oFormATO").addEventListener("click", oAdder);
+    
+    function oAdder(e) {
+        if (document.querySelector("#oItemPick").value === "defaultPick" || document.querySelector("#oQuantity").value === "") {
+            oopsie("Please Choose a Menu Item and Quantity.", "b");
+        } else {
+            
+            let oItems = [];
+            for (let i = 1; i <= document.querySelector("#oQuantity").value; i++) {
+                oItems.push(document.querySelector("#oItemPick").value);
+            }
 
-    //Create/Update localStorage array and display ordered items:
-    let subtotal = 0;
-    let ordList;
-    if (localStorage.getItem("ordList") === null) {
-        ordList = [];
-        oItems.forEach((item) => {ordList.push(item)});
-        localStorage.setItem("ordList", JSON.stringify(ordList));
-    } else {
-        ordList = JSON.parse(localStorage.getItem("ordList"));
 
-        ordList.forEach((item) => {
-            document.querySelector("#ordDsp").innerHTML += `<li>${menyou[item][1]}.......... $${menyou[item][0]}</li>`;
+            if (localStorage.getItem("ordList") === null) {
+                let ordList = [];
+                oItems.forEach((item) => {ordList.push(item)});
+                localStorage.setItem("ordList", JSON.stringify(ordList));
+                document.querySelector("#oItemPick").value = "defaultPick";
+                document.querySelector("#oQuantity").setAttribute("required", false); 
+                document.querySelector("#oQuantity").value = "";
+                
+                oopsie("Item Added to Order.", "g");
+            } else {
+                let ordList = JSON.parse(localStorage.getItem("ordList"));
+                oItems.forEach((item) => {ordList.push(item)});
+                localStorage.setItem("ordList", JSON.stringify(ordList));
+                document.querySelector("#oItemPick").value = "defaultPick";
+                document.querySelector("#oQuantity").setAttribute("required", false); 
+                document.querySelector("#oQuantity").value = "";
+                
+                oopsie("Item Added to Order.", "g");
+            }
+        }
 
-            subtotal += parseFloat(menyou[item][0]);
-        });
+        e.preventDefault();
     }
 
-    //Order totals:
+    //Display ordered items and tally subtotal:
+    let subtotal = 0;
+    let printCount = 0;
+
+    setInterval(function() {
+        if (localStorage.getItem("ordList") != null) {
+            let ordListDsp = JSON.parse(localStorage.getItem("ordList"));
+
+            ordListDsp.forEach((item) => {
+                if (ordListDsp[printCount] === item) {
+                    document.querySelector("#ordDsp").innerHTML += `<li>${menyou[item][1]}.......... $${menyou[item][0]}</li>`;
+
+                    subtotal += parseFloat(menyou[item][0]);
+                    printCount++;
+                }
+            });
+        }
+    }, 500);
+
+
+
+    //Order totals/calculations:
     let oCalc = document.querySelector("#oCalc");
     oCalc.addEventListener("click", function(e) {
-        document.querySelector("#oSubtotal").innerText = `Subtotal: $${subtotal.toFixed(2)}`;
-        document.querySelector("#oHST").innerText = `HST(15%): $${(subtotal * .15).toFixed(2)}`;
-        document.querySelector("#oTotal").innerText = `Total: $${(subtotal * 1.15).toFixed(2)}`;
+        if (subtotal * 1.15 !== 0) {
+            document.querySelector("#oSubtotal").innerText = `Subtotal: $${subtotal.toFixed(2)}`;
+            document.querySelector("#oHST").innerText = `HST(15%): $${(subtotal * .15).toFixed(2)}`;
+            document.querySelector("#oTotal").innerText = `Total: $${(subtotal * 1.15).toFixed(2)}`;
+        } else {
+            oopsie("Cannot Calculate Total With Empty Order.", "b");
+        }
 
         e.preventDefault();
     });
